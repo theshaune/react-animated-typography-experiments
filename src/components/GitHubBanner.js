441694d9466bda92437dea9d7088eb72579ee806
shaune <contact@shaunewest.com>
2017-04-10 00:27:47 +1000

    Convert GitHubBanner to styled

--- src/components/GitHubBanner.js
+++ src/components/GitHubBanner.js
@@ -1,10 +1,17 @@
 import React from 'react';
-import MotionTypography from './MotionTypography';
+import styled from 'styled-components';
+
+const Component = styled.a`
+  position: absolute;
+  top: 0;
+  right: 0;
+  border: 0;
+`
 
 const GitHubBanner = () => (
-  <a href="https://github.com/theshaune/react-animated-typography-experiments" className="GitHubBanner">
+  <Component href="https://github.com/theshaune/react-animated-typography-experiments">
     <img src="https://camo.githubusercontent.com/52760788cde945287fbb584134c4cbc2bc36f904/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f77686974655f6666666666662e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_white_ffffff.png" />
-  </a>
+  </Component>
 );
 
 export default GitHubBanner;
