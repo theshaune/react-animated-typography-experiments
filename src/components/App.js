70139f245189fcbaa3f1a530aa4a44f00e72530d
shaune <contact@shaunewest.com>
2017-04-09 16:10:03 +1000

    Add GitHub Banner and Url

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import MotionTypography from './MotionTypography';
+import GitHubBanner from './GitHubBanner';
 
 const App = () => (
   <div className="App">
+    <GitHubBanner />
     <div className="App-main">
       <MotionTypography title={`Who cares, it's only fashion!`} />
     </div>
