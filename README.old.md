14d29ce5688a0f5049d2c0c1a607eddd2ffefa56
shaune <contact@shaunewest.com>
2017-04-09 12:36:36 +1000

    file restructure

--- README.old.md
+++
@@ -1,1 +0,0 @@
-# react-animated-typography-experiments
--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import App from './components/App';
 
 ReactDOM.render(
   <App />,
   document.getElementById('root')
 );
+
+
