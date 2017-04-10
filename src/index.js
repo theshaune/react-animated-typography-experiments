54f27733ee30bd0816f2232e3999b76975b83cd2
shaune <contact@shaunewest.com>
2017-04-10 14:43:33 +1000

    Add and import normalize
    also clean out the normalise.styl

--- src/index.js
+++ src/index.js
@@ -1,11 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'normalize.css';
 import App from './components/App';
 import './styles/index.styl';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
-
-
+ReactDOM.render(<App />, document.getElementById('root'));
