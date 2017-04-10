c840fc13ab652263279de6d3473eee18743e4a78
shaune <contact@shaunewest.com>
2017-04-10 16:05:36 +1000

    Capitalise child components

--- src/components/GitHubBanner.js
+++ src/components/GitHubBanner.js
@@ -4,21 +4,21 @@ import GitHubBannerLink from './GitHubBannerLink';
 import GitHubBannerImage from './GitHubBannerImage';
 
 const GitHubBanner = () => (
-  <GitHubBanner.container>
-    <GitHubBanner.link
+  <GitHubBanner.Container>
+    <GitHubBanner.Link
       href="https://github.com/theshaune/react-animated-typography-experiments"
     >
-      <GitHubBanner.image
+      <GitHubBanner.Image
         src="https://camo.githubusercontent.com/52760788cde945287fbb584134c4cbc2bc36f904/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f77686974655f6666666666662e706e67"
         alt="Fork me on GitHub"
         dataCanonicalSrc="https://s3.amazonaws.com/github/ribbons/forkme_right_white_ffffff.png"
       />
-    </GitHubBanner.link>
-  </GitHubBanner.container>
+    </GitHubBanner.Link>
+  </GitHubBanner.Container>
 );
 
-GitHubBanner.container = GitHubBannerContainer;
-GitHubBanner.link = GitHubBannerLink;
-GitHubBanner.image = GitHubBannerImage;
+GitHubBanner.Container = GitHubBannerContainer;
+GitHubBanner.Link = GitHubBannerLink;
+GitHubBanner.Lmage = GitHubBannerImage;
 
 export default GitHubBanner;
