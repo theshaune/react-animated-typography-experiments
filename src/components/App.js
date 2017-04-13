556e926a5171ad5920b4a2accba8160322c35c04
shaune <contact@shaunewest.com>
2017-04-13 12:02:25 +1000

    Refactor App to styled-components

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,35 @@
 import React from 'react';
+import styled from 'styled-components';
 import MotionTypography from './MotionTypography';
 import GitHubBanner from './GitHubBanner';
 
+const Container = styled.div`
+  background-color: #000000;
+  color: #FFFFFF;
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  position: fixed;
+  overflow-x: none;
+  overflow-y: scroll;
+  width: 100vw;
+`;
+
+const Main = styled.div`
+  align-items: center;
+  display: flex;
+  flex-grow: 1;
+  justify-content: center;
+  padding: 1rem;
+`;
+
 const App = () => (
-  <div className="App">
+  <Container>
     <GitHubBanner />
-    <div className="App-main">
+    <Main>
       <MotionTypography title={`Who cares, it's only fashion!`} />
-    </div>
-  </div>
+    </Main>
+  </Container>
 );
 
 export default App;
